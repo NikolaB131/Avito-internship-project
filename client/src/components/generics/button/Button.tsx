1f0c27f9c9a9ae6f22babac4a0aba746160eed41
Николай <Kolya.Balancev@yandex.ru>
2022-11-13 16:14:40 +0300

    Button component rework

--- client/src/components/generics/button/Button.tsx
+++ client/src/components/generics/button/Button.tsx
@@ -2,17 +2,11 @@ import { useState } from 'react';
 import s from './Button.module.css';
 
 interface ButtonProps {
-  children: string
+  children: string,
+  clickHandler?: () => void
 }
 
-function Button({children}: ButtonProps) {
-  const [clicked, setPointerDown] = useState(false);
-
-  const clickHandler = () => {
-    setPointerDown(true);
-    console.log(12);
-  }
-
+function Button({children, clickHandler}: ButtonProps) {
   return (
     <button className={s.button} onClick={clickHandler}>
       <span>{children}</span>
